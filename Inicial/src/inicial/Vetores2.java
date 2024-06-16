c390350c3d6a4de796cdd541d7d74029717064de
jvpgjava <jjdevv05@gmail.com>
2024-06-16 18:25:04 -0300

    Curso Java

--- Inicial/src/inicial/Vetores2.java
+++ Inicial/src/inicial/Vetores2.java
@@ -1,4 +1,26 @@
 package inicial;
 
 public class Vetores2 {
+  public static void main(String[]args){
+      int[] numeros = {9, 10, 12, 25, 2};
+      int maior = numeros[0];
+      int menor = numeros[0];
+      int media = 0;
+
+      for( int i = 0; i < numeros.length; i++){
+          if (numeros[i] > maior){
+              maior = numeros[i];
+          }
+          if (numeros[i] < menor){
+              menor = numeros[i];
+          }
+          media += numeros[i]; //mesma coisa que disser media + numeros[i]
+
+      }
+      System.out.println("Maior: " + maior);
+      System.out.println("Menor: " + menor);
+      System.out.println("Média: " + media/ numeros.length);
+
+  }
+
 }
