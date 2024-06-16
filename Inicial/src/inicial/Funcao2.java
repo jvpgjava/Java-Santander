f5483c8bb1f96201ec1a9b4e83d257e6c21c9220
jvpgjava <jjdevv05@gmail.com>
2024-06-16 18:53:11 -0300

    Curso Java

---
+++ Inicial/src/inicial/Funcao2.java
@@ -0,0 +1,14 @@
+package inicial;
+
+public class Funcao2 {
+    public static void main(String[]args){
+        int resultado = soma(2, 3);
+        System.out.println("A soma é igual a " + resultado);
+
+
+    }
+    public static int soma(int a, int b){//deve receber os parametros no parenteses
+        return a + b;
+
+    }
+}
---
+++ Inicial/src/inicial/Funcoes.java
@@ -0,0 +1,16 @@
+package inicial;
+
+public class Funcoes {
+
+    public static void main(String[]args){
+        String nomeOriginal = "João"; //deve receber mesmo parametro nos parenteses também
+        saudacao(nomeOriginal);
+        //saudacao():
+    }
+    public static void saudacao(String nomeParametro){
+        System.out.println("Olá " + nomeParametro);
+        //System.out.println("Olá João");
+
+    }
+
+}
