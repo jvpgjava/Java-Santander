0d9be7eb4c37891017c7d7afd2e60a5502f012c1
jvpgjava <jjdevv05@gmail.com>
2024-06-16 15:39:34 -0300

    Curso Java

--- Inicial/src/inicial/lacosNumericos.java
+++ Inicial/src/inicial/lacosNumericos.java
@@ -1,4 +1,11 @@
 package inicial;
 
 public class lacosNumericos {
+    public static void main(String[]agrs){
+    // 1 2 3 4 5 6 7 8 9 10
+    // para uma variável que inicia em um 1 e vai até 10, mudando 1 por 1, faça:
+        for(int i = 1, i ) {
+
+        }
+   }
 }
