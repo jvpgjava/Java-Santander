8a3d440fbde600084dc1f1e7435cbc9b932c978a
jvpgjava <jjdevv05@gmail.com>
2024-06-19 23:08:23 -0300

    Curso Java

---
+++ Inicial/src/Animais/Cachorro.java
@@ -0,0 +1,180 @@
+package Animais;
+
+public class Cachorro {
+
+    //private String nome; //private só ele mesmo consegue acessar, da mesma classe
+    //protected String nome;
+//    public String nome;
+//    public String cor;
+//    public int altura;
+//    public double peso;
+//    public int tamanhoDoRabo;
+//    public String estadoDeEspirito;
+
+    //atributos
+    static int numeroDeCachorros;
+
+    private String nome;
+    private String cor;
+    private int altura;
+    private double peso;
+    private int tamanhoDoRabo;
+    private String estadoDeEspirito;
+
+    public void comer() {}
+
+    public void latir() {
+        System.out.println("AU AU");
+    }
+
+    public String pegar() {
+        return "bolinha";
+    }
+
+    //construtores, ctrl shift para selecionar todos
+    //construtor vai facilitar a criação de um objeto
+    // Cachorro(String nome, String cor, int altura, double peso, String estadoDeEspirito){
+    //      this.nome = nome; //botao direito, generate, constroi automatico
+
+    //get e set servem para proteger o objeto
+//    public String getNome() { //get serve para mexer e criar a regra que desejo, posso mostrar o que quero
+//        return nome;
+//    }
+//
+//    public void setNome(String nome) { //void não existe retorno
+//        if
+//        this.nome = nome; //quando utilizo this, eu digo para o java que quero o atributo dessa classe, e então ele identifica que o outro nome é daquele
+    //botao direito, dps em generate, constroi automatico
+
+    //construtor padrão
+    public Cachorro(){}
+
+    //construtores adicionais
+    public Cachorro(String nome, String cor, int altura, double peso, int tamanhoDoRabo, String estadoDeEspirito) {
+        this.nome = nome;
+        this.cor = cor;
+        this.altura = altura;
+        this.peso = peso;
+        this.tamanhoDoRabo = tamanhoDoRabo;
+        this.estadoDeEspirito = estadoDeEspirito;
+
+        numeroDeCachorros ++; //++ como se eu aumentasse um
+    }
+
+
+    //métodos
+
+    public static int getNumeroDeCachorros() {
+        return numeroDeCachorros;
+    }
+
+    public static void setNumeroDeCachorros(int numeroDeCachorros) {
+        Cachorro.numeroDeCachorros = numeroDeCachorros;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public int getTamanhoDoRabo() {
+        return tamanhoDoRabo;
+    }
+
+    public void setTamanhoDoRabo(int tamanhoDoRabo) {
+        this.tamanhoDoRabo = tamanhoDoRabo;
+    }
+
+    public String getEstadoDeEspirito() { //private permite que qualquer lugar que acessar esse atributo é permitido, por isso que não deu problema no switch
+        return estadoDeEspirito;
+    }
+
+    public void setEstadoDeEspirito(String estadoDeEspirito) {
+        this.estadoDeEspirito = estadoDeEspirito;
+    }
+
+    public String interagir(String acao) {
+        switch (acao) {
+            case "carinho":
+                this.estadoDeEspirito = "feliz";
+                break;
+            case "vai dormir":
+                this.estadoDeEspirito = "bravo";
+                break;
+            case "pisar na patinha":
+                this.estadoDeEspirito = "triste";
+                break;
+            default:
+                this.estadoDeEspirito = "neutro";
+        }
+        return estadoDeEspirito;
+    }
+    //botao direito e toString, faz e gera automatico os metodos toString, igual dos getter e setters
+
+    @Override
+    public String toString() {
+        return "Cachorro{" +
+                "nome='" + nome + '\'' +
+                '}';
+    }
+}
+
+//    public void setNome(String nome) { //void não existe retorno
+//        if
+//        this.nome = nome; //quando utilizo this, eu digo para o java que quero o atributo dessa classe, e então ele identifica que o outro nome é daquele
+ //botao direito, dps em generate, constroi automatico
+
+//        if (acao.equals("carinho")){
+//          this.estadoDeEspirito = "feliz";
+//    } else if (acao.equals("vai dormir")){
+//        this.estadoDeEspirito = "bravo";
+//    } else if (acao.equals("pisar na patinha")){
+//            this.estadoDeEspirito = "triste";
+//        }
+//        else{
+//            this.estadoDeEspirito = "neutro";
+//        }
+//        return estadoDeEspirito;
+//    }
+
+//+--------------+----------------------+---------------------------------------+
+//        |   Nível      | Dentro da mesma      | Fora do pacote, mas em subclasses    |
+//        |   de Acesso  | classe               | ou mesmo pacote                      |
+//        +--------------+----------------------+---------------------------------------+
+//        |   public     | Sim                  | Sim                                   |
+//        +--------------+----------------------+---------------------------------------+
+//        |   protected  | Sim                  | Sim                                   |
+//        +--------------+----------------------+---------------------------------------+
+//        |   default    | Sim                  | Não                                   |
+//        +--------------+----------------------+---------------------------------------+
+//        |   private    | Sim                  | Não                                   |
+//        +--------------+----------------------+---------------------------------------+
+
+
--- Inicial/src/POOs/Main.java
+++ Inicial/src/POOs/Main.java
@@ -1,23 +1,62 @@
 package POOs;
 
+
+import Animais.Cachorro;
+
 public class Main {
     public static void main(String[]args){
 
-        Cachorro cachorro1 = new Cachorro ();
+        //construtor
+        Cachorro cachorro1 = new Cachorro ("Puppy", "Marrom", 25, 5.5, 5, "nada");
+        Cachorro cachorro2 = new Cachorro ("Puppy", "Marrom", 25, 5.5, 5, "nada");
 
-        cachorro1.nome = "Duke" ;
-        cachorro1.cor = "Marrom";
-        cachorro1.altura = 25 ;
-        cachorro1.peso = 5.5;
-        cachorro1.tamanhoDoRabo = 5 ;
+        System.out.println(cachorro1.toString()); //vai ser exibido textos estranhos com numeros aleatorios diferentes nos dois, o que significa que na memoria temos 2 objetos diferentes
+        System.out.println(cachorro2.toString());
+
+        //se eu utilizar como variavel de referencia, vão estar no mesmo lugar os objetos:
+//        cachorro1 = cachorro2;
+        System.out.println("-----------------------");
+        System.out.println(cachorro1.toString());
+        System.out.println(cachorro2.toString());
+
+        //@Override
+        //    public String toString() {
+        //        return "Cachorro{" +
+        //                "nome='" + nome + '\'' +
+        //                '}';  Ao rodarmos esse acima que está se referenciando ao cachorro la no arquivo Cachorro percebemos que ele mostra o nome do cachorro
+
+        //é exibido o objeto no mesmo lugar:
+//        Animais.Cachorro@6acbcfc0
+//        Animais.Cachorro@5f184fc6
+//                -----------------------
+//                Animais.Cachorro@5f184fc6
+//        Animais.Cachorro@5f184fc6
+
+        //
+
+//        System.out.println(cachorro2.getNome());
+//        System.out.println(cachorro2.getPeso());
+
+//        cachorro1.nome; = "Duke" ; //momento que coloquei protected na string, não posso acessar por estar em pacote diferente
+//        cachorro1.cor = "Marrom";
+//        cachorro1.altura = 25 ;
+//        cachorro1.peso = 5.5;
+//        cachorro1.tamanhoDoRabo = 5 ;
+
+//        cachorro1.setNome("Duke"); //momento que coloquei protected na string, não posso acessar por estar em pacote diferente
+//        cachorro1.setCor("Marrom");
+//        cachorro1.setAltura(25);
+//        cachorro1.setPeso(5.5);
+//        cachorro1.setTamanhoDoRabo(5); ;
 
 //        cachorro1.latir();
 //        System.out.println("O cachorro pegou uma " + cachorro1.pegar()); //concatenando Strings
 
-        System.out.println("O cachorro está " + cachorro1.interagir("carinho")); //cachorro fica feliz
-        System.out.println("O cachorro está " + cachorro1.interagir("nada")); //cachorro fica neutro
-        System.out.println("O cachorro está " + cachorro1.interagir("vai dormir")); //cachorro fica bravo
-        System.out.println("O cachorro está " + cachorro1.interagir("pisar na patinha")); //cachorro fica triste
+//        System.out.println("O cachorro está " + cachorro1.interagir("carinho")); //cachorro fica feliz
+//        System.out.println("O cachorro está " + cachorro1.interagir("nada")); //cachorro fica neutro
+//        System.out.println("O cachorro está " + cachorro1.interagir("vai dormir")); //cachorro fica bravo
+//        System.out.println("O cachorro está " + cachorro1.interagir("pisar na patinha")); //cachorro fica triste
+
 
     }
 }
